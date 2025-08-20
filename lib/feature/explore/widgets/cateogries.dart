db42f5de0d159246ba26de83bb5261e1cab50dd0
Andrew417 <andrewehab417@gmail.com>
2025-08-20 16:59:41 +0300

    fixed color opacity

--- lib/feature/explore/widgets/cateogries.dart
+++ lib/feature/explore/widgets/cateogries.dart
@@ -12,45 +12,45 @@ class Category {
   });
 }
 
-const List<Category> categories = [
+List<Category> categories = [
   Category(
     title: 'Fresh Fruits\n& Vegetables',
-    backgroundColor: Color(0xFFFFE5E5),
+    backgroundColor: Color(0xFFFFE5E5).withValues(alpha: 0.8),
     image: 'assets/images/fruit_and_vegitables.svg',
   ),
   Category(
     title: 'Cooking Oil\n& Ghee',
-    backgroundColor: Color(0xFFFFF2CC),
+    backgroundColor: Color(0xFFFFF2CC).withValues(alpha: 0.8),
     image: 'assets/images/oil.svg',
   ),
   Category(
     title: 'Meat & Fish',
-    backgroundColor: Color(0xFFD5E8D4),
+    backgroundColor: Color(0xFFD5E8D4).withValues(alpha: 0.8),
     image: 'assets/images/meat_&_fish.svg',
   ),
   Category(
     title: 'Bakery & Snacks',
-    backgroundColor: Color(0xFFFFD9B3),
+    backgroundColor: Color(0xFFFFD9B3).withValues(alpha: 0.8),
     image: 'assets/images/bakey_&_snacks.svg',
   ),
   Category(
     title: 'Dairy & Eggs',
-    backgroundColor: Color(0xFFE0EBF5),
+    backgroundColor: Color(0xFFE0EBF5).withValues(alpha: 0.8),
     image: 'assets/images/eggs.svg',
   ),
   Category(
     title: 'Beverages',
-    backgroundColor: Color(0xFFE6CCFF),
+    backgroundColor: Color(0xFFE6CCFF).withValues(alpha: 0.8),
     image: 'assets/images/beverages.svg',
   ),
   Category(
     title: 'Cooking Oil\n& Ghee',
-    backgroundColor: Color(0xFFFFF2CC),
+    backgroundColor: Color(0xFFFFF2CC).withValues(alpha: 0.8),
     image: 'assets/images/oil.svg',
   ),
   Category(
     title: 'Meat & Fish',
-    backgroundColor: Color(0xFFD5E8D4),
+    backgroundColor: Color(0xFFD5E8D4).withValues(alpha: 0.8),
     image: 'assets/images/meat_&_fish.svg',
   ),
 ];
